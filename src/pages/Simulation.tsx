a1b672bd4047d7d1fed906d77e5954b006da3d09
a5-001 <orvill.delatorre@atom5engineering.com>
2025-12-01 13:44:09 -0500

    -

--- src/pages/Simulation.tsx
+++ src/pages/Simulation.tsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import Navigation from "@/components/Navigation";
-// import heroBackground from "@/assets/hero-background.jpg";
+import heroBackground from "@/assets/hero-background.jpg";
 import { Maximize2, Minimize2 } from "lucide-react";
 import { Button } from "@/components/ui/button";
 
