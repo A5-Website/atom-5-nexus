b227d0e95a7f733a084bd0f64fcd236ca649a699
a5-001 <orvill.delatorre@atom5engineering.com>
2025-12-07 12:36:43 -0500

    -

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -1,4 +1,4 @@
-3import { NavLink } from "@/components/NavLink";
+import { NavLink } from "@/components/NavLink";
 import { useLocation } from "react-router-dom";
 import logo from "@/assets/logo.png";
 
