7a2504373c61aa413ec75f063e16e9c14260e29c
a5-001 <orvill.delatorre@atom5engineering.com>
2025-12-07 11:46:05 -0500

    -

--- src/main.tsx
+++ src/main.tsx
@@ -1,17 +1,6 @@
-// import { createRoot } from "react-dom/client";
-// import App from "./App.tsx";
-// import "./index.css";
-
-// createRoot(document.getElementById("root")!).render(<App />);
-
-
 import { createRoot } from "react-dom/client";
+import App from "./App.tsx";
 import "./index.css";
 
-const rootElement = document.getElementById("root")!;
+createRoot(document.getElementById("root")!).render(<App />);
 
-createRoot(rootElement).render(
-  <h1 style={{ color: "red", textAlign: "center", marginTop: "50px" }}>
-    main.tsx is working!
-  </h1>
-);
