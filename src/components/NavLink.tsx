1b52fe4d43c4f919e04abc669452900adb96a7d8
a5-001 <orvill.delatorre@atom5engineering.com>
2025-12-07 12:54:28 -0500

    -

--- src/components/NavLink.tsx
+++ src/components/NavLink.tsx
@@ -1,6 +1,6 @@
 import { NavLink as RouterNavLink, NavLinkProps } from "react-router-dom";
 import { forwardRef } from "react";
-import { cn } from "@/lib/utils";
+import { cn } from "@/lib/utils"; // your utility for classNames
 
 interface NavLinkCompatProps extends Omit<NavLinkProps, "className"> {
   className?: string;
@@ -9,18 +9,18 @@ interface NavLinkCompatProps extends Omit<NavLinkProps, "className"> {
 }
 
 const NavLink = forwardRef<HTMLAnchorElement, NavLinkCompatProps>(
-  ({ className, activeClassName, pendingClassName, to, ...props }, ref) => {
+  ({ className = "", activeClassName = "", pendingClassName = "", to, ...props }, ref) => {
     return (
       <RouterNavLink
         ref={ref}
         to={to}
         className={({ isActive, isPending }) =>
-          cn(className, isActive && activeClassName, isPending && pendingClassName)
+          cn(className, isActive ? activeClassName : "", isPending ? pendingClassName : "")
         }
         {...props}
       />
     );
-  },
+  }
 );
 
 NavLink.displayName = "NavLink";
