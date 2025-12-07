8a881a55404b6d9c763ea3728ec652348da15044
a5-001 <orvill.delatorre@atom5engineering.com>
2025-12-07 11:54:07 -0500

    -

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,6 @@
-// import { createRoot } from "react-dom/client";
-// import Home from "./pages/Home.tsx";  // point directly to Home
-// import "./index.css";
-
-// createRoot(document.getElementById("root")!).render(<Home />);
-
 import { createRoot } from "react-dom/client";
+import Home from "./pages/Home.tsx";  // point directly to Home
 import "./index.css";
 
-const rootElement = document.getElementById("root")!;
+createRoot(document.getElementById("root")!).render(<Home />);
 
-createRoot(rootElement).render(
-  <h1 style={{ color: "red", textAlign: "center", marginTop: "50px" }}>
-    main.tsx is working!
-  </h1>
-);
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,13 +1,26 @@
-import Navigation from "@/components/Navigation";
-import { BackgroundPaths } from "@/components/ui/background-paths";
+// import Navigation from "@/components/Navigation";
+// import { BackgroundPaths } from "@/components/ui/background-paths";
 
-const Home = () => {
-  return (
-    <div className="relative min-h-screen">
-      <Navigation />
-      <BackgroundPaths title="Bridging Carbon and Silicon Intelligence" />
-    </div>
-  );
-};
+// const Home = () => {
+//   return (
+//     <div className="relative min-h-screen">
+//       <Navigation />
+//       <BackgroundPaths title="Bridging Carbon and Silicon Intelligence" />
+//     </div>
+//   );
+// };
 
-export default Home;
+// export default Home;
+
+
+
+import { createRoot } from "react-dom/client";
+import "./index.css";
+
+const rootElement = document.getElementById("root")!;
+
+createRoot(rootElement).render(
+  <h1 style={{ color: "red", textAlign: "center", marginTop: "50px" }}>
+    Home.tsx is working!
+  </h1>
+);
