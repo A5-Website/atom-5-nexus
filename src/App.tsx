5037c630d3f3969402e100fa116c3ae6990c357b
a5-001 <orvill.delatorre@atom5engineering.com>
2025-12-08 23:52:31 -0500

    -

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,22 @@
-import { Toaster } from "@/components/ui/toaster";
-import { Toaster as Sonner } from "@/components/ui/sonner";
-import { TooltipProvider } from "@/components/ui/tooltip";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Home from "./pages/Home";
-import AISolutions from "./pages/AISolutions";
-import Research from "./pages/Research";
-import Simulation from "./pages/Simulation";
-import Contact from "./pages/Contact";
-import NotFound from "./pages/NotFound";
+import Navigation from "@/components/Navigation";
 
-const queryClient = new QueryClient();
+import Home from "@/pages/Home";
+import Research from "@/pages/Research";
+import AISolutions from "@/pages/AISolutions";
+import Contact from "@/pages/Contact";
 
-const App = () => (
-  <QueryClientProvider client={queryClient}>
-    <TooltipProvider>
-      <Toaster />
-      <Sonner />
-      <BrowserRouter basename="/atom-5-nexus">
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/ai-solutions" element={<AISolutions />} />
-          <Route path="/research" element={<Research />} />
-          <Route path="/simulation" element={<Simulation />} />
-          <Route path="/contact" element={<Contact />} />
-          {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
-          <Route path="*" element={<NotFound />} />
-        </Routes>
-      </BrowserRouter>
-    </TooltipProvider>
-  </QueryClientProvider>
-);
+export default function App() {
+  return (
+    <BrowserRouter>
+      <Navigation />
 
-export default App;
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/research" element={<Research />} />
+        <Route path="/ai-solutions" element={<AISolutions />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
